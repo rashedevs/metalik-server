2fae8e75b1746a59f8038683542f67adc40af4f8
rashedevs <rasheduap2015@gmail.com>
2022-05-23 23:00:00 +0600

    create dynamic get endpoint to find specific tool by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,13 @@ async function run() {
       const cursor = toolCollection.find(query);
       const tools = (await cursor.toArray()).reverse();
       res.send(tools);
+    });
+    // find one by id
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolCollection.findOne(query);
+      res.send(tool);
     });
     // load reviews from db
     app.get("/review", async (req, res) => {
