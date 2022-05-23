77b4689f8a5fb94a97693b294801b70a5c70e0a3
rashedevs <rasheduap2015@gmail.com>
2022-05-23 19:00:32 +0600

    add post endpoint to send new reviews to database

--- index.js
+++ index.js
@@ -21,11 +21,25 @@ async function run() {
   try {
     await client.connect();
     const toolCollection = client.db("metalik").collection("tools");
+    const reviewCollection = client.db("metalik").collection("reviews");
     app.get("/tool", async (req, res) => {
       const query = {};
       const cursor = toolCollection.find(query);
-      const tools = await cursor.toArray();
+      const tools = (await cursor.toArray()).reverse();
       res.send(tools);
+    });
+    // load reviews from db
+    app.get("/review", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = (await cursor.toArray()).reverse();
+      res.send(reviews);
+    });
+    // post review from ui to db
+    app.post("/review", async (req, res) => {
+      const newReview = req.body;
+      const result = await reviewCollection.insertOne(newReview);
+      res.send(result);
     });
   } finally {
     // await client.close();
