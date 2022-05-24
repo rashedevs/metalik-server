e256fd76ad1b17c31e00894971b96fef0c20b3c3
rashedevs <rasheduap2015@gmail.com>
2022-05-24 13:19:26 +0600

    create delete api to delete specific order

--- index.js
+++ index.js
@@ -78,6 +78,21 @@ async function run() {
       const result = await orderCollection.insertOne(newOrder);
       res.send(result);
     });
+    // my orders from order collection
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    // delete a specific order finding by id
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
