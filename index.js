d27b061cfd5d261bdef3804407f57ba8220abd95
rashedevs <rasheduap2015@gmail.com>
2022-05-25 13:04:26 +0600

    delete api for manage product is implemented

--- index.js
+++ index.js
@@ -176,6 +176,13 @@ async function run() {
       const result = await orderCollection.deleteOne(query);
       res.send(result);
     });
+    // delete a specific product finding by id
+    app.delete("/tool/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await toolCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
