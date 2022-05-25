0da7a9d63b94e72a953dea5b625599871f475867
rashedevs <rasheduap2015@gmail.com>
2022-05-25 12:05:49 +0600

    verify admin middleware function used to protect the admin routes

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ async function run() {
     const reviewCollection = client.db("metalik").collection("reviews");
     const orderCollection = client.db("metalik").collection("orders");
     const userCollection = client.db("metalik").collection("users");
+
+    // verify admin middleware function
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "Forbidden" });
+      }
+    };
     // load all tools
     app.get("/tool", async (req, res) => {
       const query = {};
@@ -48,13 +61,13 @@ async function run() {
       res.send(tools);
     });
     // post a new tool
-    app.post("/tool", async (req, res) => {
+    app.post("/tool", verifyJWT, verifyAdmin, async (req, res) => {
       const newTool = req.body;
       const result = await toolCollection.insertOne(newTool);
       res.send(result);
     });
     // get all users
-    app.get("/user", verifyJWT, async (req, res) => {
+    app.get("/user", verifyJWT, verifyAdmin, async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const users = await cursor.toArray();
@@ -85,22 +98,14 @@ async function run() {
       res.send({ admin: isAdmin });
     });
     // put admin role
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({
-        email: requester,
-      });
-      if (requesterAccount.role === "admin") {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: "admin" },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      } else {
-        res.status(403).send({ message: "Forbidden" });
-      }
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
     // find one by id
     app.get("/tool/:id", async (req, res) => {
@@ -109,7 +114,7 @@ async function run() {
       const tool = await toolCollection.findOne(query);
       res.send(tool);
     });
-    // single tool update api
+    // single tool quantity update api
     app.put("/tool/:id", async (req, res) => {
       const id = req.params.id;
       const updatedTool = req.body;
